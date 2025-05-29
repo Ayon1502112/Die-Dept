5bbc5f0786d1b0428f950efb1aca104c0dadef62
Ayon1502112 <152854568+Ayon1502112@users.noreply.github.com>
2025-05-29 10:44:37 +0600

    Code.py

--- Code.py
+++ Code.py
@@ -6,6 +6,7 @@ st.title("নিচের button গুলো click করে, app গুলো 
 urls = {
     "Monthly All Die Maintenance (Dashboard)": "https://lookerstudio.google.com/reporting/6d689733-0d3c-4667-ac7f-ee96cd4f0523",
     "(Entry Form) Maintenance": "https://www.appsheet.com/start/a81e8891-6548-4994-80c5-d7ec4f7d8c29",
+    "For Foundry Maintenance": "https://www.appsheet.com/start/66f77481-5c17-4959-8f4d-847319939c9a",
     "Maintenance (Dashboard)": "https://lookerstudio.google.com/reporting/5a415e63-ac18-46d1-bfc9-78eb60f9b943"
 }
 

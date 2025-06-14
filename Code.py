2598837f544f432b749bf04cbb4c2ac38cd993ce
Ayon1502112 <152854568+Ayon1502112@users.noreply.github.com>
2025-06-14 20:41:26 +0600

    Code.py

--- Code.py
+++ Code.py
@@ -114,13 +114,7 @@ all_urls = {
     "Monthly All Die Maintenance (Dashboard)": {
         "url": "https://lookerstudio.google.com/reporting/6d689733-0d3c-4667-ac7f-ee96cd4f0523"
     },
-    "(Entry Form) Maintenance": {
-        "url": "https://www.appsheet.com/start/a81e8891-6548-4994-80c5-d7ec4f7d8c29"
-    },
-    "For Foundry Maintenance": {
-        "url": "https://www.appsheet.com/start/66f77481-5c17-4959-8f4d-847319939c9a"
-    },
-    "Maintenance (Dashboard)": {
+    "Only Urgent Maintenance (Dashboard)": {
         "url": "https://lookerstudio.google.com/reporting/5a415e63-ac18-46d1-bfc9-78eb60f9b943"
     }
 }
